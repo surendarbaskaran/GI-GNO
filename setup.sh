1556b207a2cbe967394b52a3f1d484f78857391a
surendarbaskaran <baskarsupra@gmail.com>
2026-01-02 17:59:31 +0000

    updated requirement.txt, added setup.sh for easier starting

---
+++ setup.sh
@@ -0,0 +1,3 @@
+pip uninstall -y torch torchvision torchaudio
+pip install torch==2.4.0 torchvision torchaudio
+pip install torch-scatter torch-sparse torch-geometric -f https://data.pyg.org/whl/torch-2.4.0+cu121.html
